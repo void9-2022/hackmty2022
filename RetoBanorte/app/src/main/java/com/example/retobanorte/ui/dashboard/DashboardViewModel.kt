b25217a03049566c1a852874b0c99653d4534ece
David A. Lozano <void9david@gmail.com>
2022-09-25 06:19:26 -0500

    6:19

--- RetoBanorte/app/src/main/java/com/example/retobanorte/ui/dashboard/DashboardViewModel.kt
+++ RetoBanorte/app/src/main/java/com/example/retobanorte/ui/dashboard/DashboardViewModel.kt
@@ -7,11 +7,9 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-<<<<<<< HEAD
         value = ""
-=======
         value = "Transferencia"
->>>>>>> 21122353571384cfd4e13eade14308ca07410ddf
+
     }
     val text: LiveData<String> = _text
 }
